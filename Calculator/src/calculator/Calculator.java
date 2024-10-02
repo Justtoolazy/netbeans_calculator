ee2b3f34615c3abc5d819189fe9efd490cc39214
John Lyndon M. Ronquillo <ronquillojohnlyndon@gmail.com>
2024-10-02 16:07:00 +0800

    Frame

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+import javax.util.Jframe;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       frame_calc frame = new frame_calc();
+       frame.setVisible(true);
+    }
+    
+}
